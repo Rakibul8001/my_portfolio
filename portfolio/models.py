a8a27d5b912ed7db28bd376f254493f510b57516
Md. Rakibul Islam <mdrakibul.islam8001@gmail.com>
2021-01-14 22:13:59 +0600

    added resume model

--- portfolio/models.py
+++ portfolio/models.py
@@ -2,4 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+class Resume(models.Model):
+    name = models.CharField(max_length=20)
+    title = models.CharField(max_length=50)
+    cv_file = models.FileField(upload_to='document')
     
+    def __str__(self):
+        return self.title
+
+
