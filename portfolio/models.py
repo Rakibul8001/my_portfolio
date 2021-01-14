f90609f96697bbf4fa44fdbce76ae471e0b116bc
Md. Rakibul Islam <mdrakibul.islam8001@gmail.com>
2021-01-14 22:15:23 +0600

    about model

--- portfolio/models.py
+++ portfolio/models.py
@@ -10,4 +10,13 @@ class Resume(models.Model):
     def __str__(self):
         return self.title
 
+class About(models.Model):
+    name = models.CharField(max_length=20)
+    image = models.ImageField(upload_to='portfolio')
+    description = models.TextField(max_length=1000)
+    
+    def __str__(self):
+        return self.name
 
+
+   
